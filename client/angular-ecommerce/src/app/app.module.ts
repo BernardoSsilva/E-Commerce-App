eb69cc87496e18e60c1368d7c0d2e232a165ac27
Berenicio <bernardo.ssilva0808@gmail.com>
2023-06-06 07:27:30 -0300

    Created the products list component and service to call the spring api

--- client/angular-ecommerce/src/app/app.module.ts
+++ client/angular-ecommerce/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  // ao colocar ProductService ou outros serviços em providers permitimos a injeção da dependencia em outras classes
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://8080/api/products'
+
+  constructor(private httpClient: HttpClient) { }
+
+  // metodo para puxar a lista de produtos
+  // esse metodo vai mapear o Json e retornar uma matriz observavel
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+    
+  }
+}
+
+interface GetResponse{
+  _embedded: {
+    products : Product[];
+  }
+}
