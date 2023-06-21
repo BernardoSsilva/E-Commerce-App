4be2bdb3fe74f6db3476fe971411d3d45f1ea9f7
Berenicio <bernardo.ssilva0808@gmail.com>
2023-06-21 06:08:44 -0300

    routes setted

--- client/angular-ecommerce/src/app/app.module.ts
+++ client/angular-ecommerce/src/app/app.module.ts
@@ -9,6 +9,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { CurrencyPipe } from '@angular/common';
 
+import { Routes, RouterModule } from '@angular/router'
+
+const routes : Routes = [
+  {path: 'category/:id', component: ProductListComponent}
+  {path: 'category', component: ProductListComponent}
+  {path: 'products', component: ProductListComponent}
+  {path: '', redirectTo: '/products', pathMatch: 'full'}
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
