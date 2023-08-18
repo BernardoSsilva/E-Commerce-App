2840f91e5665ee50e0e8a7b10f0d112fd5da7a73
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2023-08-18 02:32:50 -0300

    fixed configurations

--- E-commerceSpring/src/main/java/com/projects/EcommerceSpring/config/MyDataResConfig.java
+++ E-commerceSpring/src/main/java/com/projects/EcommerceSpring/config/MyDataResConfig.java
@@ -1,8 +1,9 @@
 package com.projects.EcommerceSpring.config;
+import com.projects.EcommerceSpring.entity.Product;
+import com.projects.EcommerceSpring.entity.ProductCategory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +12,9 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.metamodel.EntityType;
-import com.projects.EcommerceSpring.entity.Product;
-import com.projects.EcommerceSpring.entity.ProductCategory;
-
-// esta classe configura os repositorios, limitando-os a apenas leitura, por meio da desabilitação dos metodos Post, Put e Delete
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class MyDataResConfig implements RepositoryRestConfigurer {
@@ -24,44 +22,51 @@ public class MyDataResConfig implements RepositoryRestConfigurer {
     private EntityManager entityManager;
 
     @Autowired
-    public MyDataResConfig(EntityManager theEntityManager){
+    public void MyDataRestConfig(EntityManager theEntityManager) {
         entityManager = theEntityManager;
     }
+
+    // esta classe configura os repositorios, limitando-os a apenas leitura, por meio da desabilitação dos metodos Post, Put e Delete
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH };
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        // disable HTTP methods for Product: PUT, POST and DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // metodo interno adicional para retornar id
+        // call an internal helper method
         exposeIds(config);
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
 
+       // metodo interno adicional para retornar id
+        //
+
+        // get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        List<Class> entityClasses= new ArrayList<>();
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
 
-        for(EntityType tempEntityType : entities){
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
         }
 
+        // - expose the entity ids for the array of entity/domain types
         Class[] domainTypes = entityClasses.toArray(new Class[0]);
-
         config.exposeIdsFor(domainTypes);
     }
-
 }
